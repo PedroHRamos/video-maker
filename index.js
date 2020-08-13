0a8c985e46252a537d28375941700f0ce02115f2
Pepedro00721 <pepedro00721@gmail.com>
2020-08-12 22:59:23 -0300

    Busca de Imagens no google images

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const robots = {
     input: require('./robots/input.js'),
     text: require('./robots/text.js'),
-    state: require('./robots/state.js')
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js')
 }
 
 async function start(){
@@ -9,6 +10,7 @@ async function start(){
     try{
         robots.input()
         await robots.text();
+        await robots.image();
 
         const content = robots.state.load();
         console.dir(content,{deph: null});
