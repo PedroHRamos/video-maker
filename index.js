c5e50e6b32ee5647458fba5290adb615bcab226e
Pepedro00721 <pepedro00721@gmail.com>
2020-08-17 20:50:43 -0300

    download-images-fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function start(){
         await robots.image();
 
         const content = robots.state.load();
-        console.dir(content,{deph: null});
+        //console.dir(content,{deph: null});
     }catch(e){
         console.log(e)
     }
--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader');
 const google = require('googleapis').google;
 const customSearch = google.customsearch('v1');
 const state = require('./state.js');
@@ -28,7 +29,7 @@ async function robot(){
             cx: googleSearchCredentials.searchEngineId,
             q: query,
             searchType: 'image',
-            num: 2
+            num: 3
         });
 
         const imagesUrl = response.data.items.map((item) =>{
